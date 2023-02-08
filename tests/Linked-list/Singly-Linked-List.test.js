62c1650d51e21545e3357ec173a7a7cdf464b918
mmdzov <mzov939@gmail.com>
2023-02-08 15:30:45 +0330

    Added tests

--- tests/Linked-list/Singly-Linked-List.test.js
+++ tests/Linked-list/Singly-Linked-List.test.js
@@ -1,9 +1,9 @@
 const SinglyLinkedList = require("../../src/2.Linked-List/2_1.Singly-Linked-List/app");
 
 describe("Singly Linked List", () => {
-  it("should add nodes", () => {
-    const ll = new SinglyLinkedList();
+  const ll = new SinglyLinkedList();
 
+  it("should add nodes", () => {
     ll.add(10);
     ll.add(12);
     ll.add(13);
@@ -11,5 +11,12 @@ describe("Singly Linked List", () => {
     expect(ll.getHead().value).toBe(10);
     expect(ll.getHead().next.value).toBe(12);
     expect(ll.getHead().next.next.value).toBe(13);
+  });
+
+  it("should delete node", () => {
+    const removed = ll.remove(12);
+
+    expect(removed).toBe(12);
+    expect(ll.getHead().next.value).toBe(13);
   });
 });
